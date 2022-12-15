22c9400128577b48e420d7dd641cb14c22216694
Estíbaliz Martín <estibalizmares@gmail.com>
2022-12-15 18:44:11 +0100

    Initial commit

---
+++ src/main/java/com/example/serverJustPoteito/ServerJustPoteitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverJustPoteito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerJustPoteitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerJustPoteitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serverJustPoteito/ServerJustPoteitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverJustPoteito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerJustPoteitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
