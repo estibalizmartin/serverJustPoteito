56d638ac6274c5130557903eef0c552ca19b91b2
DavidPereiroAmez <davidpereiroamez@gmail.com>
2022-12-20 21:03:22 +0100

    Login and register finished

--- src/main/java/com/example/serverJustPoteito/security/JwtTokenFilter.java
+++ src/main/java/com/example/serverJustPoteito/security/JwtTokenFilter.java
@@ -1,5 +1,7 @@
 package com.example.serverJustPoteito.security;
 
+import com.example.serverJustPoteito.auth.model.Rol;
+import com.example.serverJustPoteito.auth.persistence.Role;
 import com.example.serverJustPoteito.auth.persistence.User;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -15,6 +18,7 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Set;
 
 
 @Component
@@ -64,7 +68,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 	private void setAuthenticationContext(String token, HttpServletRequest request) {
 		//Falta corregir esto
 		UserDetails userDetails = getUserDetails(token);
-		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, null);
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 		authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
@@ -73,9 +77,9 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 	private UserDetails getUserDetails(String token) {
 		//Falta corregir esto
 		User userDetails = new User();
-		String[] jwtSubject = jwtUtil.getSubject(token).split(",");
-		userDetails.setId(Integer.parseInt(jwtSubject[0]));
-		userDetails.setEmail(jwtSubject[1]);
+		userDetails.setId(jwtUtil.getUserId(token));
+		userDetails.setEmail(jwtUtil.getSubject(token));
+		userDetails.setRoles(jwtUtil.getUserRoles(token));
 		return userDetails;
 	}
 }
