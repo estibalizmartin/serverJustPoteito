684b0b7d3157a2931a6453e124ea4f5b213eb012
DavidPereiroAmez <davidpereiroamez@gmail.com>
2022-12-20 21:03:22 +0100

    signup and login done

--- src/main/java/com/example/serverJustPoteito/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/example/serverJustPoteito/security/CustomAuthenticationEntryPoint.java
@@ -11,11 +11,10 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        if(response.getStatus() == HttpServletResponse.SC_FORBIDDEN){
-            //token válido pero usuario sin permisos
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Authorization failed");
-        }else{
-            //token no válido o inexistente
+
+        if (response.getStatus() >= 400) {
+            response.sendError(response.getStatus());
+        } else {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization failed");
         }
     }
